0157d89cd1283cab54e0745c9a6ac611c8fa67fa
Oscar M <omsanchez@improveit.mx>
2014-12-01 15:20:54 -0600

    Fix files

--- src/portal/app/js/controllers.js
+++ src/portal/app/js/controllers.js
@@ -2,10 +2,4 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+angular.module('taskApp.controllers', []);
