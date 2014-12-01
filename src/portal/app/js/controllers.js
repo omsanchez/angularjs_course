16d7f80dc857e8e92dbce7b26f6ab2c3db3e064f
Oscar M <omsanchez@improveit.mx>
2014-12-01 16:46:42 -0600

    Step-1b

--- src/portal/app/js/controllers.js
+++ src/portal/app/js/controllers.js
@@ -2,4 +2,35 @@
 
 /* Controllers */
 
-angular.module('taskApp.controllers', []);
+angular.module('taskApp.controllers', [])
+.controller('TaskCtrl', function($scope){
+	$scope.tasks = [
+		{
+			'code': 'TSK01',
+			'description' : 'Implementar la notificación de la creación de una borrador de reporte de falla',
+			'user' : {
+				'name': 'Beatriz',
+				'lastname': 'Torres' 
+			},
+			'status': 'Planeada'
+		},
+		{
+			'code': 'TSK02',
+			'description' : 'Agregar la unidad de medida del número de parte',
+			'user' : {
+				'name': 'Alonso',
+				'lastname': 'Roque' 
+			},
+			'status': 'En proceso'
+		},
+		{
+			'code': 'TSK03',
+			'description' : 'Implementar Envio de Cotización por parte del proveedor',
+			'user' : {
+				'name': 'Oscar',
+				'lastname': 'Sánchez' 
+			},
+			'status': 'Completada'
+		}
+	];
+})
