7f586ad8afe50c4bbc9be0fcef9b90a8af4f2c86
Oscar M <omsanchez@improveit.mx>
2014-12-01 21:19:11 -0600

    Directive templateUrl

--- src/portal/app/js/directives.js
+++ src/portal/app/js/directives.js
@@ -6,7 +6,6 @@
 angular.module('taskApp.directives', [])
 .directive('taskItem',  function() {
 	return {
-		restrict: 'AC',
-	  template: '<span> {{ task.code }} - {{ task.description }} </span> <p> Asignado a {{ task.user.name }} {{task.user.lastname }} </p> <p> Estatus: {{ task.status }} </p> '
+	  templateUrl: 'task-item.html'
 	};
 });
